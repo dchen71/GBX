f74e439b56c85f4b360d4b51da2cc7dc461a04d3
Dan <dchen12812@gmail.com>
2016-05-15 21:33:55 -0700

    use strict jaavscript

--- index.android.js
+++ index.android.js
@@ -1,34 +1,34 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ GBX
+ A generalized skeleton app to display information from Granblue Fantasy or any other mobile game
  */
 
+//Strict javascript delcaration
+"use strict";
+
+//Load necessary react components
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-class GBX extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+//Main page
+//Probably just show new events at top
 
+//Quest Page
+//Lists quests
+
+//Character page
+//Something to list characters
+
+//Tooltip
+//Some kind of push alert at x time to let know angel time
+
+//Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,4 +48,24 @@ const styles = StyleSheet.create({
   },
 });
 
+class GBX extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+
+//Build app
 AppRegistry.registerComponent('GBX', () => GBX);
